f5e355e74c6d5b732e3a0f4a38c4587198dfaf5b
Mahima Gangavarapu <39587007+gmahima@users.noreply.github.com>
2019-10-17 21:35:46 +0530

    add company field to job

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,4 +1,11 @@
 //const { gql } = require("apollo-server-express");
 
-const Query = { jobs: () => [] };
-module.exports = { Query };
+//const { gql } = require("apollo-server-express");
+const db = require("./db");
+const Query = {
+  jobs: () => db.jobs.list()
+};
+const Job = {
+  company: job => db.companies.get(job.companyId)
+};
+module.exports = { Query, Job };
