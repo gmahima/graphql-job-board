d22a59134631b21f79b891fe2f81f2a7b082284f
Mahima Gangavarapu <39587007+gmahima@users.noreply.github.com>
2019-10-17 21:27:17 +0530

    add company field

--- server/resolvers.js
+++ server/resolvers.js
@@ -3,4 +3,7 @@ const db = require("./db");
 const Query = {
   jobs: () => db.jobs.list()
 };
-module.exports = { Query };
+const Job = {
+  company: job => db.companies.get(job.companyId)
+};
+module.exports = { Query, Job };
