16bc1a1a70397d68baab1faf9e12161e73005409
Mahima Gangavarapu <39587007+gmahima@users.noreply.github.com>
2019-10-17 20:58:59 +0530

    add jobs query

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,4 +1,7 @@
 //const { gql } = require("apollo-server-express");
 
-const Query = { greeting: () => "I love anime and programming:)" };
+const Query = {
+  jobs: () => [],
+  greeting: () => "I love anime and programming:)"
+};
 module.exports = { Query };
