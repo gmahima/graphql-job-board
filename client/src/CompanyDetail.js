b10b26668b2dfa4862183bd941beaaec0fa671c7
Mahima Gangavarapu <39587007+gmahima@users.noreply.github.com>
2019-10-18 13:20:24 +0530

    add error handling and company

--- client/src/CompanyDetail.js
+++ client/src/CompanyDetail.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
-import { companies } from './fake-data';
+import React, { Component } from "react";
+import { loadCompany } from "./requests.js";
 
 export class CompanyDetail extends Component {
-  constructor(props) {
-    super(props);
-    const {companyId} = this.props.match.params;
-    this.state = {company: companies.find((company) => company.id === companyId)};
+  //const {companyId} = this.props.match.params;
+
+  state = {
+    company: {}
+  };
+  async componentDidMount() {
+    const { companyId } = this.props.match.params;
+    const company = await loadCompany(companyId);
+    this.setState({
+      company
+    });
   }
 
   render() {
-    const {company} = this.state;
+    const { company } = this.state;
     return (
       <div>
         <h1 className="title">{company.name}</h1>
